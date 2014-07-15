0a0e0dceb73e7b8c63d0ec933587601564278844
Steve <steve.sykora@gmail.com>
2014-07-14 21:10:11 -0400

    add folders

--- MCP and Forge/forge/src/main/java/com/crownedone14/letsmod/LetsMod.java
+++ MCP and Forge/forge/src/main/java/com/crownedone14/letsmod/LetsMod.java
@@ -1,7 +1,31 @@
 package com.crownedone14.letsmod;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = "letsmod", name = "Let's Mod", version = "1.7.2-a1.0")
-public class LetsMod {
+public class LetsMod
+{
+    @Mod.Instance("LetsMod")
+    public static LetsMod instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
 }
